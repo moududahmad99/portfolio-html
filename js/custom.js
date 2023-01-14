9af1319e880d6f0a208d6227bf05311be2efa73a
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-01-14 21:59:58 +0600

    Responsive 60% complete  Home Area

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,9 @@
 $(function(){
-    
+    $('#bar-icon').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.home-navNprofile').toggleClass('toggle');
+    });
 });
+let changeIcon = function (icon) {
+    icon.classList.toggle('fa-xmark')
+}
